d96f1e640cae272e29cba2f07b7541946a9be97f
Mulayam <arvind@netforceinfotech.com>
2016-08-12 19:16:41 +0530

    login done

--- app/src/main/java/com/netforceinfotech/tripsplit/MainActivity.java
+++ app/src/main/java/com/netforceinfotech/tripsplit/MainActivity.java
@@ -1,13 +1,34 @@
 package com.netforceinfotech.tripsplit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.netforceinfotech.tripsplit.Profile.ProfileActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button sign_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        sign_button = (Button) findViewById(R.id.sign_button);
+
+        sign_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+
+                startActivity(i);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/netforceinfotech/tripsplit/Profile/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.netforceinfotech.tripsplit.Profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.netforceinfotech.tripsplit.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
