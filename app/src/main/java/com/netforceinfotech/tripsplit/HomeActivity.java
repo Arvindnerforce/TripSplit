84bb084cb16072fba4d81ddb52efb2ddb0b2f720
Mulayam <arvind@netforceinfotech.com>
2016-08-16 19:23:00 +0530

    menu design

--- app/src/main/java/com/netforceinfotech/tripsplit/HomeActivity.java
+++ app/src/main/java/com/netforceinfotech/tripsplit/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.netforceinfotech.tripsplit;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,31 +17,89 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class HomeActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import com.netforceinfotech.tripsplit.Profile.PagerAdapter;
+import com.netforceinfotech.tripsplit.general.WrapContentViewPager;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
+{
+
+    CoordinatorLayout coordinatorLayout;
+    WrapContentViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        setupTab();
     }
 
+
+    private void setupTab()
+    {
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorlayout);
+
+        viewPager = (WrapContentViewPager)findViewById(R.id.pager);
+        viewPager.setPagingEnabled(false);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+
+        tabLayout.addTab(tabLayout.newTab().setIcon(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_plane)));
+        tabLayout.addTab(tabLayout.newTab().setIcon(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_car)));
+        tabLayout.addTab(tabLayout.newTab().setIcon(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_bus)));
+        tabLayout.addTab(tabLayout.newTab().setIcon(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_build)));
+
+        final int tabIconColor = ContextCompat.getColor(getApplicationContext(), R.color.black);
+        final int tabIconSelectedColor = ContextCompat.getColor(getApplicationContext(), R.color.white);
+
+        tabLayout.getTabAt(0).getIcon().setColorFilter(tabIconSelectedColor, PorterDuff.Mode.SRC_IN);
+        tabLayout.getTabAt(1).getIcon().setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN);
+        tabLayout.getTabAt(2).getIcon().setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN);
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+    }
+
+
     @Override
-    public void onBackPressed() {
+    public void onBackPressed()
+    {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
---
+++ app/src/main/java/com/netforceinfotech/tripsplit/Profile/PagerAdapter.java
@@ -0,0 +1,42 @@
+package com.netforceinfotech.tripsplit.Profile;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.netforceinfotech.tripsplit.Profile.flight.FlightFragment;
+
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+    private FlightFragment flight;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs)
+    {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position)
+        {
+            case 0:
+                flight = new FlightFragment();
+                return flight;
+            case 1:
+                flight = new FlightFragment();
+                return flight;
+            default:
+                flight = new FlightFragment();
+                return flight;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
